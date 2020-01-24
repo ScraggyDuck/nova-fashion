cfccd05bd614f295177d1e9363ca126f47d4e8f9
JokerV <Quocvietcoi2000@gmail.com>
2020-01-24 21:42:56 +0700

    Collection

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { HomeComponent } from "./home.component";
 import { HomeRoutingModule } from "./home.routing";
 import { BannerModule } from "src/app/components/banner/banner.module";
+import { CollectionModule } from "src/app/components/collection/collection.module";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, BannerModule],
+  imports: [CommonModule, HomeRoutingModule, BannerModule, CollectionModule],
   exports: [HomeComponent]
 })
 export class HomeModule {}
